259c46a05c4637f8372cdcdcd19ecf4ceb702e28
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-09-29 10:02:22 +0300

    fill name

--- tests/test_submit_student_registration_form.py
+++ tests/test_submit_student_registration_form.py
@@ -5,3 +5,6 @@ from selene.support.shared import browser
 def test_submit_student_registration_form():
     #open url
     browser.open('/automation-practice-form')
+    # Fill name
+    browser.element('#firstName').should(be.blank).click().type('Charles')
+    browser.element('#lastName').should(be.blank).click().type('Leclerc')
