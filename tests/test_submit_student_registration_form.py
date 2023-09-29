1f4bec351cfced583f806cf0ba3f09856ff7b031
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-09-29 10:10:55 +0300

    fill gender

--- tests/test_submit_student_registration_form.py
+++ tests/test_submit_student_registration_form.py
@@ -8,3 +8,7 @@ def test_submit_student_registration_form():
     # Fill name
     browser.element('#firstName').should(be.blank).click().type('Charles')
     browser.element('#lastName').should(be.blank).click().type('Leclerc')
+    # Fill email
+    browser.element('#userEmail').should(be.blank).click().type('CL16@test.com')
+    # Fill gender
+    browser.element('[for="gender-radio-1"]').click()
