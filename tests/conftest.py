dc7138f778ab9c77b35adfe326ef0778690290ab
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-09-29 16:29:29 +0300

    change window size

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,8 @@ def set_browser():
     browser.config.base_url = 'https://demoqa.com'
     #browser.config.window_width = 1920
     #browser.config.window_height = 1020
-    browser.config.driver.maximize_window()
+    #browser.config.driver.maximize_window()
+    browser.config.timeout = 60
 
     yield
 
